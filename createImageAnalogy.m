1f35c9f3c3150e6b3dfae9825edbcb102ff87d0d
Dave Yan <fdu5526@live.com>
2014-12-01 22:31:44 -0500

    initial commit

---
+++ createImageAnalogy.m
@@ -0,0 +1,7 @@
+function [output] = createImageAnalogy(A, Ap, B)
+
+	Bp = zeros(size(B));
+
+
+	output = Bp;
+end
---
+++ transfer.m
@@ -0,0 +1,22 @@
+function [done] = transfer()
+	% load data
+	data = loadRiver();
+	A = data{1}; Ap = data{2}; B = data{3};
+
+	% create the image analogy
+	Bp = createImageAnalogy(A, Ap, B);
+
+	% show/save the image
+	%imshow(Bp);
+
+end
+
+
+
+% load the river data
+function [data] = loadRiver()
+	A = im2double(imread('input/riverA.jpg'));
+	Ap = im2double(imread('input/riverAp.jpg'));
+	B = im2double(imread('input/riverB.jpg'));
+	data = {A,Ap,B};
+end
